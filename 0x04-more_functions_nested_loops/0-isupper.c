3504d2ee693f8ec3853f746942347eca48851180
celestine <nwabuisicelestine@gmail.com>
2022-03-20 20:21:47 +0100

    removed white spaces

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
+
 {
 if (c >= 'A' && c <= 'Z')
 {
