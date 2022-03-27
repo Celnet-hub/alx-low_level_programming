83073e166691518a6fa8d0f341b44a817273935d
celestine <nwabuisicelestine@gmail.com>
2022-03-28 00:01:23 +0100

    added commets

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,7 +11,7 @@ int i;
 char str[8] = "_putchar";
 for (i = 0; i < 8; i++)
 {
-_putchar(str[i]);
+_putchar(str[i]); /* prints the characters in an index position */
 }
 _putchar('\n');
 return (0);
