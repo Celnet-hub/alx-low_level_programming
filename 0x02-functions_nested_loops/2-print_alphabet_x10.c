d20a33516d1c058ced3cc055285655ad71ad0ca0
celestine <nwabuisicelestine@gmail.com>
2022-03-28 01:24:09 +0100

    modified file to only print 10 characters.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,15 +3,15 @@
 
 void print_alphabet_x10(void)
 {
-    int c;
-    char low_case;
-    for ( c = 0; c <= 10; c++)
-    {
-        for(low_case = 'a'; low_case <= 'z'; low_case++)
-        {
-            _putchar(low_case);
-        }
-        _putchar('\n');
-    }
-    
+int c;
+char low_case;
+for ( c = 0; c < 10; c++)
+{
+for(low_case = 'a'; low_case <= 'z'; low_case++)
+{
+_putchar(low_case);
+}
+_putchar('\n');
+}
+
 }
