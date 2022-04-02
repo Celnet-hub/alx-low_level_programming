cc488298ad1d29ad4b596e9d3f04a2877e0e68e1
celestine <nwabuisicelestine@gmail.com>
2022-04-02 15:22:58 +0100

    added description for parameter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - is a function that checks for lowercase letters
- * Parameter: C is the character to be checked
+ * Parameter: c is the character to be checked
  * Return: 1 if c is a lowercase letter, 0 otherwise
  *
  */
