8d28ee7bc2d2ad378fb03bc1c4582d767d9320c7
celestine <nwabuisicelestine@gmail.com>
2022-04-03 06:27:55 +0100

    creates a file that prints signs based on the value of the parameter passed to a function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints signs based on the value of n
+ * 
+ * @n: is the int parameter passed to the function 
+ * Return: 1 if n > 0
+ *         0 if n == 0
+ *         -1 if n < 0 
+ */
+
+int print_sign(int n)
+{    
+if (n > 0)
+{
+_putchar('+');
+return(1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return(0);
+}
+else if (n < 0)
+{
+_putchar('-');
+return(-1);
+}
+
+
+}
