0d7bba084adf6f0d3c706ce8a4faeeb1987d8448
celestine <nwabuisicelestine@gmail.com>
2022-04-03 06:40:50 +0100

    removed trailing whitespaces

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,14 +2,14 @@
 
 /**
  * print_sign - function that prints signs based on the value of n
- * @n: is the int parameter passed to the function 
+ * @n: is the int parameter passed to the function
  * Return: 1 if n > 0
  *         0 if n == 0
- *         -1 if n < 0 
+ *         -1 if n < 0
  */
 
 int print_sign(int n)
-{    
+{
 if (n > 0)
 {
 _putchar('+');
