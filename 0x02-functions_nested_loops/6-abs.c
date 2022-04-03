73cf1ec4a9480b211dfab2c5b720cb6171146403
celestine <nwabuisicelestine@gmail.com>
2022-04-03 17:23:07 +0100

    created a c program that prints the absolute value of a parameter

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * _abs(int) is a function that returns the absolute value of int
+ *@c is the parameter that is passed to the function
+ * 
+ * Returns: the absolute value of the parameter passed. 
+ * 
+ */
+
+int _abs(int c)
+{
+int r;
+r = abs(c);
+
+return r;
+}
