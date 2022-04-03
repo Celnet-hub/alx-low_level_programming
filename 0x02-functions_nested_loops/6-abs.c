dacba92c09645ba8125ecf07fb91c2e6f9fea6d7
celestine <nwabuisicelestine@gmail.com>
2022-04-03 17:25:55 +0100

    removed  trailing whitespaces

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,15 +4,13 @@
 /**
  * _abs(int) is a function that returns the absolute value of int
  *@c is the parameter that is passed to the function
- * 
- * Returns: the absolute value of the parameter passed. 
- * 
+ * Returns: the absolute value of the parameter passed.
+ *
  */
 
 int _abs(int c)
 {
 int r;
 r = abs(c);
-
 return r;
 }
