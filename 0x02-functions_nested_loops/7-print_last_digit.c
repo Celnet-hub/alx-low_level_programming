8ee488545aab3b435fdaa167cd61ae2a5a305468
celestine <nwabuisicelestine@gmail.com>
2022-04-04 13:43:29 +0100

    removed last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * print_last_digit: prints the last digit of a number
+ * print_last_digit - last digit
  * @c: is the int that get passed as a parameter
  * Return: int.
  */
@@ -13,7 +13,7 @@ int print_last_digit(int c)
 int last_digit;
 if (c < 0)
 {
-last_digit = (-1*(c % 10 ));
+last_digit = (-1 * (c % 10));
 _putchar(last_digit + '0');
 return (last_digit);
 }
