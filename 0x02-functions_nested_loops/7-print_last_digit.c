f947d1004d97ab8d31e0baa519eba82e850dd561
celestine <nwabuisicelestine@gmail.com>
2022-04-04 13:40:48 +0100

    removed white spaces

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,15 +13,15 @@ int print_last_digit(int c)
 int last_digit;
 if (c < 0)
 {
-last_digit = (-1 * (c % 10 ));
+last_digit = (-1*(c % 10 ));
 _putchar(last_digit + '0');
-return(last_digit);
+return (last_digit);
 }
 else
 {
 last_digit = c % 10;
 _putchar(last_digit + '0');
-return(last_digit);
+return (last_digit);
 }
 
 
