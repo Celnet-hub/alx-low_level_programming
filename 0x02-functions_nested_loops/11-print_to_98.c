32833af494f969c7550defc7755b3a7d893144fb
celestine <nwabuisicelestine@gmail.com>
2022-04-11 02:48:03 +0100

    removed last semi-colon

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,19 +12,21 @@ void print_to_98(int n)
 {
 if (n < 98)
 {
-while (n <= 98)
+while (n < 98)
 {
 printf("%d, ", n);
 n++;
 }
+printf("%d", 98);
 }
 else if (n > 98)
 {
-while (n >= 98)
+while (n > 98)
 {
 printf("%d, ", n);
 n--;
 }
+printf("%d\n", 98);
 }
 else
 {
