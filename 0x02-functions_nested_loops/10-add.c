400543bc9ebec89db6e4fe8200263b29aa36ec42
celestine <nwabuisicelestine@gmail.com>
2022-04-11 01:57:13 +0100

    removed trailing white spaces

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * add: function that adds two integers
- * @a: is the first parameter passed to the function
- * @b: is the second parameter passed to the function
- * Return: the sum of the two parameters
- * 
+ * add: function that adds two numbers
+ *@a: is the first parameter passed to the function
+ *@b: is the second parameter passed to the function
+ *Return: the sum of the two parameters
  */
 
 int add(int a, int b)
