329999a17a58789d87e409c227e3e19fe2507dd1
celestine <nwabuisicelestine@gmail.com>
2022-04-11 02:00:17 +0100

    added a short description of the function to conform with Betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * add: function that adds two numbers
+ * add -function that adds two numbers
  *@a: is the first parameter passed to the function
  *@b: is the second parameter passed to the function
  *Return: the sum of the two parameters
