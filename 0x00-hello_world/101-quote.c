e722d25329e98e0a7cf15ccb8279755e4979ce3b
Chidubem <nwabuisicelestine@gmail.com>
2022-10-12 04:20:07 +0100

    betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include<unistd.h>
 
 /**
- * main - prints the standard error sandwiched around the standard outpu/t
- *
- * Return: returns 1
- */
+* main - prints the standard error sandwiched around the standard output
+*
+* Return: returns 1
+*/
 
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
