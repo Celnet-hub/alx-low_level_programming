3f1f529b6f170fa4bbde97339e5e82caef27196c
Chidubem <nwabuisicelestine@gmail.com>
2022-10-12 16:49:27 +0100

    created a program that creates a multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,55 +1,49 @@
 #include "main.h"
-
 /**
- * print_times_table - prints times table for numbers from 0-14
- * @n: An input integer value
- * Return: Nothing
+ * print_times_table  -prints tables
+ *@n: print int
+ * Return: nothing
  */
 void print_times_table(int n)
 {
-	int i, j;
-
-	if (n > 0 && n < 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('0');
-			for (j = 1; j <= n; j++)
-				putformat(i * j);
-			_putchar('\n');
-		}
-	}
-}
-
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
- */
-void putformat(int n)
+int a, b, c; /*Declaring statements*/
+if (n < 16 && n > -1)
 {
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
+for (a = 0; a <= n; a++)/*First for*/
+{
+for (b = 0; b <= n; b++)/*Secondo for*/
+{
+c = a * b;/* multiplication a*b*/
+if ((c / 10) == 0)/*If the first digit is 0*/
+{
+if  (b == 0)
+{ _putchar ('0');
 }
+if (b != 0)/*print if b is different 0*/
+{
+_putchar (' ');
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+} /*End the first IF*/
+else if ((c / 10) <= 9 && ((c % 10) <= 9))/*print to 99*/
+{
+_putchar (' ');
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+} /*End ELSE IF*/
+else/*print from 100*/
+{
+_putchar ((c / 100) + '0');
+_putchar (((c / 10) % 10)  + '0');
+_putchar ((c % 10) + '0');
+} /*End ELSE*/
+if (b < n) /*don't print the last (;)*/
+{
+_putchar(',');
+_putchar (' ');
+}
+} /*End second FOR*/ _putchar ('\n');
+} /*End first FOR*/
+}
+} /*End int main*/
