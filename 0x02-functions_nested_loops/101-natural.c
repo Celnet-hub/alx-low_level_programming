d519bc3d6eb5c06c71df6a659e7f473baa575367
Chidubem <nwabuisicelestine@gmail.com>
2022-10-12 18:15:17 +0100

    print natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * Description - Write a program that computes and prints the sum of all the
+ * multiples of 3 or 5 below 1024 (excluded), followed by a new line.
+ * main - increasing and decreasing until 98
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i, result; /*Delcaring statements*/
+
+i = 0;
+result = 0;
+
+while (i < 1024)
+{
+if (i % 3 == 0 || i % 5 == 0) /*Start condition IF*/
+result += i;
+i++;
+} 
+
+printf("%d\n", result);
+return (0);
+}
